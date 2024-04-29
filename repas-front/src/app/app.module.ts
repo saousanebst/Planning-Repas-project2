60abd2cb9b42afee8484d3ac7778000311680124
HediB <hedibardou@gmail.com>
2024-04-29 18:25:35 +0200

    modification home et css

--- repas-front/src/app/app.module.ts
+++ repas-front/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UtilisateurComponent
     
   ],
   imports: [
