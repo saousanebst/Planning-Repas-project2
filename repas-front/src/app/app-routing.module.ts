ca5a152a55592faf8389e76b2045acc771f690d5
saousaneBst <bastaoui.saousane@gmail.com>
2024-04-29 16:24:17 +0200

    modification app-component

--- repas-front/src/app/app-routing.module.ts
+++ repas-front/src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: "home", component: HomeComponent},
-  {path: "", redirectTo: "home", pathMatch: "full"},
-  //{path: "**", component: PageNotFoundComponent}
+ 
 ];
 
 @NgModule({
--- repas-front/src/app/app.module.ts
+++ repas-front/src/app/app.module.ts
@@ -3,17 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { UtilisateurComponent } from './utilisateur/utilisateur.component';
+
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    NavBarComponent,
-    UtilisateurComponent
+    AppComponent
+    
   ],
   imports: [
     BrowserModule,
