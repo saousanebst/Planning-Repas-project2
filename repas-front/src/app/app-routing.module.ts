b4558eb86745d3ba5741dc39ad15396b18f8ac2f
HediB <hedibardou@gmail.com>
2024-04-30 17:55:22 +0200

    angular routage + lien navbar

--- repas-front/src/app/app-routing.module.ts
+++ repas-front/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { EntreeComponent } from './entree/entree.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
- 
+  {path: "home", component: HomeComponent},
+  {path: "inscription", component: InscriptionComponent},
+  {path: "ingredient", component: IngredientComponent},
+  {path: "entree", component: EntreeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "login", component: LoginComponent},
+  {path: "", redirectTo: "home", pathMatch: "full"}
 ];
 
 @NgModule({
--- repas-front/src/app/app.component.ts
+++ repas-front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root, [app-root]',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'repas-front';
+
+  constructor(private authService: AuthService) {
+
+  }
+  
+  isLogged(): boolean {
+    return this.authService.isLogged();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+  
 }
